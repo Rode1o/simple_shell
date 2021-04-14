a13086387dbd0c97797e2c9ffbd09ab86ac7d64a
jj131204 <jj131204@gmail.com>
2021-04-14 01:56:04 +0000

    shell

--- main.c
+++ main.c
@@ -11,19 +11,19 @@ int main(void)
 	char *line = NULL;
 	size_t bufsize = 0;
 	int num;
-	char *str = "#C_isfun$ ";
+	/*char *str = "#C_isfun$ ";*/
 	char *tok[1024];/*valor cualquira*/
 	int validators = 1;
 
-	/*char buffer[1024];*/
+	char buffer[1024];
 
 	signal(SIGINT, sigint_handler);
 	while (1)
 	{
 		if (isatty(STDIN_FILENO))
 		{
-			/*printf("%s ", getcwd(buffer, 1024));*/
-			write(STDOUT_FILENO, str, 10);
+			printf("%s~ ", getcwd(buffer, 1024));
+			/*write(STDOUT_FILENO, str, 10);*/
 		}
 
 		num = getline(&line, &bufsize, stdin);
